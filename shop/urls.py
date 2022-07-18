b3fa8d8b3eccbd3f4d68b445d3fde6554a9c4715
preeti kumari kushwaha <43378294+preetikushwaha1@users.noreply.github.com>
2022-07-18 21:13:18 +0530

    update

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from shop import views
 
 urlpatterns = [ 
-        path("", views.index, name ="ShopHome")
+        path("", views.index, name ="ShopHome"),
+        path("about/", views.aboutUs, name="AboutUs"),
+        path("contact/", views.contact, name="ContactUs"),
+        path("tracker", views.tracker, name="TrackingStatus"),
+        path("search/", views.search, name="Search"),
+        path("productview/", views.productView, name="ProductView"),
+        path("checkout/", views.checkout, name="CheckOut"),
 ]
