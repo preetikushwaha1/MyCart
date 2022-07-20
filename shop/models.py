eab25f29f69145d784dc987e2510dd94f5a575e4
preeti kumari kushwaha <43378294+preetikushwaha1@users.noreply.github.com>
2022-07-20 20:18:20 +0530

    update

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,9 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_id = models.AutoField
+    product_name = models.CharField(max_length=50)
+    desc = models.CharField(max_length=300)
+    pub_date = models.DateField("")
