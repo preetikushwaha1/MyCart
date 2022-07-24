0e44c7675ba62081aa4827882a30da1bb5c0fbb9
preeti kumari kushwaha <43378294+preetikushwaha1@users.noreply.github.com>
2022-07-24 21:12:47 +0530

    update

--- shop/views.py
+++ shop/views.py
@@ -2,10 +2,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
     return render(request, "shop/index.html")
-
 
 #==========About Us ===========================#
 def aboutUs(request):
