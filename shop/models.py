a73917896e716929396b7c6ebc4f74952704fa5a
preeti kumari kushwaha <43378294+preetikushwaha1@users.noreply.github.com>
2022-07-24 21:00:32 +0530

    update

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,5 @@
+from email.policy import default
+from statistics import mode
 from unittest.util import _MAX_LENGTH
 from django.db import models
 
@@ -5,5 +7,14 @@ from django.db import models
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=50)
+    category= models.CharField(max_length=50, default="")
+    sub_category = models.CharField(max_length=50, default="")
+    price = models.IntegerField(default=0)
     desc = models.CharField(max_length=300)
     pub_date = models.DateField("")
+    image = models.ImageField(upload_to="shop/images", default="")
+
+    # below line will return product name to the objects
+    def __str__(self):
+        return self.product_name            #ex: Ghadi , watch 
+
