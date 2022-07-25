105232e5777afc397e7cf02b8f499b8a39a2be44
preeti kumari kushwaha <43378294+preetikushwaha1@users.noreply.github.com>
2022-07-25 21:15:35 +0530

    upload

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,19 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from shop.models import Product
+from math import ceil
 
 
 # Create your views here.
 def index(request):
-    return render(request, "shop/index.html")
+    Products = Product.objects.all()
+    print(Products)
+    n = len(Products)       # 6
+    nSlide = n // 4 + ceil( (n/4)- (n//4))
+    print(nSlide)
+    params ={'no_of_slide':nSlide, 'range': range(nSlide), 'products': Products}
+    return render(request, "shop/index.html", params)
 
 #==========About Us ===========================#
 def aboutUs(request):
