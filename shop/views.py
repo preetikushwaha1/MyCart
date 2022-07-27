85c29f65ed158780d34765816d5e3d1e72f5f0b5
preeti kumari kushwaha <43378294+preetikushwaha1@users.noreply.github.com>
2022-07-27 21:02:09 +0530

    update

--- shop/views.py
+++ shop/views.py
@@ -9,15 +9,27 @@ from math import ceil
 def index(request):
     Products = Product.objects.all()
     print(Products)
-    n = len(Products)       # 6
-    nSlide = n // 4 + ceil( (n/4)- (n//4))  #6//4 + ceil((6/4)-(6//4)) = 0+ ceil(2 - 0) = 2
-    print(nSlide)
+   # n = len(Products)       # 6
+    #nSlide = n // 4 + ceil( (n/4)- (n//4))  #6//4 + ceil((6/4)-(6//4)) = 0+ ceil(2 - 0) = 2
+    #print(nSlide)"""
+
+    all_prods =[]
+    category_prods = Product.objects.values('id', 'category')
+    print(category_prods)
+    categories = { item['category'] for item in category_prods}
+    for cat in categories:
+        prods = Product.objects.filter(category = cat)
+        n= len(Products)
+        nSlide = n//4 + ceil((n/4)- (n//4))
+        all_prods.append([prods, range(1, nSlide), nSlide])
+        print(cat)
+
     #params ={'no_of_slide':nSlide, 'range': range(1,nSlide), 'products': Products}  #list 1
 
     #==== Now will create a list of list
-    all_prods = [ [Products, range(1, nSlide), nSlide ],
-                    [Products, range(1, nSlide), nSlide ] ]
-                    
+    """all_prods = [ [Products, range(1, nSlide), nSlide ],
+                    [Products, range(1, nSlide), nSlide ] ]"""
+
     return render(request, "shop/index.html", {'all_prods': all_prods})
 
 #==========About Us ===========================#
