8a82e13fecd05ffcc816c78796913eae2afb6744
preeti kumari kushwaha <43378294+preetikushwaha1@users.noreply.github.com>
2022-09-04 20:49:12 +0530

    updates

--- blog/views.py
+++ blog/views.py
@@ -4,13 +4,15 @@ from .models import BlogPost
 # Create your views here.
 
 def index(request):
-    return render(request, "blog/index.html")
+    mypost = BlogPost.objects.all()
+    print(mypost)
+    return render(request, "blog/index.html", {'mypost':mypost})
 
 
 
 def blogPost_fun(request, id):
 
     post = BlogPost.objects.filter(post_id = id)[0]       # fetch data from data base
-    print(post)
+    #print(post)
 
     return render(request, "blog/blog_post.html", {'post':post})
